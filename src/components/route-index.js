8706a7271c5f5f568fcd29961f7c4cb099f8eca2
Peter Simonsson <peter@simonsson.com>
2018-11-24 01:21:06 +0200

    Add fade-in animation on index route

--- src/components/route-index.js
+++ src/components/route-index.js
@@ -1,27 +1,42 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Animated } from 'react-native';
 import { Subscribe } from 'unstated';
 import RouterContainer from '../state/router';
 import ListButton from './list-button';
 
-export default function RouteIndex() {
-	return (
-		<Subscribe to={[RouterContainer]}>
-			{router => (
-				<View style={styles.container}>
-					<Text style={styles.title}>Welcome!</Text>
-					<ListButton
-						onPress={() => {
-							router.goToScanner();
-						}}
-						style={styles.button}
+export default class RouteIndex extends React.Component {
+	state = {
+		fadeAnim: new Animated.Value(0),
+	};
+
+	componentDidMount() {
+		Animated.timing(this.state.fadeAnim, {
+			toValue: 1,
+			duration: 650,
+		}).start();
+	}
+
+	render() {
+		return (
+			<Subscribe to={[RouterContainer]}>
+				{router => (
+					<Animated.View
+						style={[styles.container, { opacity: this.state.fadeAnim }]}
 					>
-						<Text style={styles.buttonText}>Start</Text>
-					</ListButton>
-				</View>
-			)}
-		</Subscribe>
-	);
+						<Text style={styles.title}>Welcome!</Text>
+						<ListButton
+							onPress={() => {
+								router.goToScanner();
+							}}
+							style={styles.button}
+						>
+							<Text style={styles.buttonText}>Start</Text>
+						</ListButton>
+					</Animated.View>
+				)}
+			</Subscribe>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
