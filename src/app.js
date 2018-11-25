a22aad9818b12ec48d8016ce7c2c28185ddeaebf
Peter Simonsson <peter@simonsson.com>
2018-11-25 04:16:40 +0200

    Add back RouteCarts

--- src/app.js
+++ src/app.js
@@ -13,6 +13,7 @@ import PersistRouterState from './components/persist-router-state';
 import RouteIndex from './components/route-index';
 import RouteAccount from './components/route-account';
 import RouteScanner from './components/route-scanner';
+import RouteCarts from './components/route-carts';
 import RouteViewCart from './components/route-view-cart';
 import RouteNotFound from './components/route-not-found';
 
