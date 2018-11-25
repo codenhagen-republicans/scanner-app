f0605d6117a6128aab71e0e42be6e2ecc2985880
Peter Simonsson <peter@simonsson.com>
2018-11-25 09:14:37 +0200

    Fix pie chart

--- src/components/pie-chart.js
+++ src/components/pie-chart.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import Config from 'react-native-config';
 import { PieChart } from 'react-native-svg-charts';
 
 const randomColor = () =>
@@ -34,10 +35,14 @@ export default class PieFootprint extends React.PureComponent {
 			);
 
 			const body = await res.json();
-			const foods = body.foods.map(food => ({
-				...food,
-				color: randomColor(),
-			}));
+
+			const foods = body.foods
+				.filter(food => food.foot_print > 0)
+				.map(food => ({
+					...food,
+					key: food.ean,
+					color: randomColor(),
+				}));
 
 			this.setState({
 				loading: false,
@@ -54,7 +59,7 @@ export default class PieFootprint extends React.PureComponent {
 		let data;
 
 		if (this.state.loading === true) {
-			<View style={{ height: 200 }} />;
+			<View style={{ height: 160 }} />;
 		}
 
 		if (
@@ -64,27 +69,63 @@ export default class PieFootprint extends React.PureComponent {
 			return <></>;
 		}
 
-		const pieData = this.state.foods
-			.filter(food => food.footprint > 0)
-			.map((food, index) => ({
-				value: food.footprint,
-				svg: {
-					fill: food.color,
-					onPress: () => console.log('press', index),
-				},
-				key: `pie-${index}`,
-			}));
+		const pieData = this.state.foods.map((food, index) => ({
+			value: food.foot_print,
+			svg: {
+				fill: food.color,
+				onPress: () => console.log('press', index),
+			},
+		}));
 
 		return (
-			<View style={styles.chart}>
-				<PieChart style={{ height: 200, width: 200 }} data={pieData} />
+			<View style={styles.container}>
+				<PieChart style={styles.chart} data={pieData} />
+				<FlatList
+					data={this.state.foods}
+					style={styles.list}
+					renderItem={this.renderItem}
+				/>
 			</View>
 		);
 	}
+
+	renderItem = ({ item: food }) => {
+		return (
+			<View key={food.ean} style={styles.listItem}>
+				<View
+					style={{
+						width: 16,
+						height: 16,
+						marginRight: 5,
+						backgroundColor: food.color,
+					}}
+				/>
+				<Text>{food.name}</Text>
+			</View>
+		);
+	};
 }
 
 const styles = StyleSheet.create({
+	container: {
+		flexDirection: 'row',
+		justifyContent: 'flex-start',
+		paddingTop: 20,
+		paddingLeft: 20,
+		paddingRight: 20,
+		paddingBottom: 5,
+		height: 200,
+	},
 	chart: {
-		justifyContent: 'center',
+		width: 150,
+		height: 150,
+		marginRight: 10,
+	},
+	list: {
+		height: 200,
+	},
+	listItem: {
+        flexDirection: 'row',
+        marginBottom: 4,
 	},
 });
